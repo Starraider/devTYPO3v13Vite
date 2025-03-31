204478d4332c0f0ae056c1017ff5b92231022e1b
Sven Kalbhenn <sven@skom.de>
2025-03-31 17:59:44 +0800

    refactor: modularize color mode functionality and update asset references in skombase13

---
+++ packages/skombase13/vite.config.js
@@ -0,0 +1,11 @@
+export default {
+    build: {
+        rollupOptions: {
+            input: {
+                main: 'packages/skombase13/Resources/Private/JavaScript/Main.entry.js',
+                // ... other entry points
+            }
+        }
+    }
+    // ... other config options
+}
